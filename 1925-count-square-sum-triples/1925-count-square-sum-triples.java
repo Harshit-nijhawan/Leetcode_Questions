571ca4fe40553c4ab630bc1302c4d2c0eb7999cb
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2025-12-08 09:10:24 +0530

    Time: 5 ms (95.82%), Space: 43.7 MB (13.99%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,20 +1,38 @@
 class Solution {
     public int countTriples(int n) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        boolean square[] = new boolean[n * n + 1];
         int count = 0;
         for(int i = 1;i<=n;i++){
-            ans.add(i * i);
+            square[i * i] = true;
         }
-        for(int i = 0;i<ans.size();i++){
-            for(int j = 0;j<ans.size();j++){
-                for(int k = 0;k<ans.size();k++){
-                    if(ans.get(i)+ans.get(j) == ans.get(k)){
+
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+               int sum = i * i + j * j;
+
+                if(sum <= n * n && square[sum]) {
                     count++;
                 }
-                }
-                
             }
         }
         return count;
+
+
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // int count = 0;
+        // for(int i = 1;i<=n;i++){
+        //     ans.add(i * i);
+        // }
+        // for(int i = 0;i<ans.size();i++){
+        //     for(int j = 0;j<ans.size();j++){
+        //         for(int k = 0;k<ans.size();k++){
+        //             if(ans.get(i)+ans.get(j) == ans.get(k)){
+        //             count++;
+        //         }
+        //         }
+                
+        //     }
+        // }
+        // return count;
     }
 }
