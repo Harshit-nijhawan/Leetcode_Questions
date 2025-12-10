0b10f09c1278919ca9b1637193a0a01edfed6c4a
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2025-12-10 15:48:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (14.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,24 @@
 class Solution {
     public int fib(int n) {
-        int dp[] = new int[n+1];
-        return helper(n,dp);
-    }
-    public int helper(int n , int dp[]){
         if(n == 0 || n == 1) return n;
-        if(dp[n] != 0) return dp[n];
-        dp[n] = helper(n - 2,dp) + helper(n -1,dp);
-        return dp[n];
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[i] = dp[i - 1]  + dp[i - 2];
+        }
+        return dp[n];  
     }
 }
+// class Solution {
+//     public int fib(int n) {
+//         int dp[] = new int[n+1];
+//         return helper(n,dp);
+//     }
+//     public int helper(int n , int dp[]){
+//         if(n == 0 || n == 1) return n;
+//         if(dp[n] != 0) return dp[n];
+//         dp[n] = helper(n - 2,dp) + helper(n -1,dp);
+//         return dp[n];
+//     }
+// }
