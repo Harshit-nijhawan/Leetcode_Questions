044d3c050c4b09bd9f37a80054342cd0d0942108
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2025-12-11 11:46:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (21.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -6,12 +6,12 @@ class Solution {
         int dp2[] = new int[n+1];
         Arrays.fill(dp,-1);
         Arrays.fill(dp2,-1);
-        int tookFirst = helper(nums,0,n-1,dp);
-        int notTookFirst = helper(nums,1,n,dp2);
+        int tookFirst = helper(nums,0,n-2,dp);
+        int notTookFirst = helper(nums,1,n-1,dp2);
         return Math.max(tookFirst,notTookFirst);
     }
     public int helper(int nums[],int i,int n,int dp[]){
-        if(i >= n) return 0;
+        if(i > n) return 0;
         if(dp[i] != -1) return dp[i];
 
         int steal = nums[i] + helper(nums,i+2,n,dp);
