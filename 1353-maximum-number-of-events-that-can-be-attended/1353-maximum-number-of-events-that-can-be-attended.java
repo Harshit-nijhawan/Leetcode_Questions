2db93cc809cc06a7140c3350449f33d5b1b6e16d
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2025-12-13 22:55:01 +0530

    Time: 73 ms (16.85%), Space: 103.5 MB (5.1%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.java
@@ -2,12 +2,15 @@ class Solution {
     public int maxEvents(int[][] event) {
         int n = event.length;
         if(n == 0) return 0;
+        //sort according to the start day of the event
         Arrays.sort(event,Comparator.comparingInt(o -> o[0]));
         int day = 0;
         int i = 0;
         int count = 0;
+        //Min heap to add the end day of the event and that shuld be sorted in increasing order
         PriorityQueue<Integer> pq =new PriorityQueue<>();
         while(!pq.isEmpty() || i < n){
+            //to start with the day of the event on which i is
             if(pq.isEmpty()){
                 day = event[i][0];
             }
@@ -20,6 +23,7 @@ class Solution {
                 count++;
             }
             day++;
+            //to skip the events which are less than the day we are
             while(!pq.isEmpty()  && pq.peek() < day){
                 pq.poll();
             }
