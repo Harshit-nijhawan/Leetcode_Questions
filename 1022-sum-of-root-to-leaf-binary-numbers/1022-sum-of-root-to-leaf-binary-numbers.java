34f1be73069ecf02968fe687eb7b7a819ca7fd42
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-02-24 10:33:55 +0530

    Time: 0 ms (100%), Space: 43.6 MB (84.1%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -14,19 +14,33 @@
  * }
  */
 class Solution {
-    public int sumRootToLeaf(TreeNode root) {
-        String str = "";
-        return helper(root,str);
+    // public int sumRootToLeaf(TreeNode root) {
+    //     String str = "";
+    //     return helper(root,str);
         
+    // }
+    // public int helper(TreeNode root,String str){
+    //     if(root == null) return 0;
+    //     str = str + root.val;
+    //     if(root.left == null && root.right == null){
+    //         return Integer.parseInt(str,2);
+    //     }
+    //     int leftSum = helper(root.left,str);
+    //     int rightSum = helper(root.right,str);
+
+    //     return leftSum + rightSum;
+    // }
+    public int sumRootToLeaf(TreeNode root) {
+        return helper(root,0);
     }
-    public int helper(TreeNode root,String str){
+    public int helper(TreeNode root,int curr ){
         if(root == null) return 0;
-        str = str + root.val;
+        curr = curr * 2 + root.val;
         if(root.left == null && root.right == null){
-            return Integer.parseInt(str,2);
+            return curr;
         }
-        int leftSum = helper(root.left,str);
-        int rightSum = helper(root.right,str);
+        int leftSum = helper(root.left,curr);
+        int rightSum = helper(root.right,curr);
 
         return leftSum + rightSum;
     }
