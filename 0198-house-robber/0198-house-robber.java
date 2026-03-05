8110a9e9a8b2f00046f6a6a2ec682ef0b7053a49
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-03-05 14:33:37 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int dp [] = new int[n+1];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for(int i = 2;i<=n;i++){
-            int steal = nums[i -1] + dp[i -2];
-            int notSteal = dp[i - 1];
-            dp[i] = Math.max(steal,notSteal);
-        }
-        return dp[n];
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return helper(nums,0,nums.length,dp);
+    }
+    public int helper(int nums[],int st,int n,int dp[]){
+        if(st >= n) return 0;
+        if(dp[st] != -1) return dp[st];
+        int take = nums[st] + helper(nums,st+2,n,dp);
+        int notTake = helper(nums,st+1,n,dp);
+
+        return dp[st] = Math.max(take,notTake);
     }
 }
-// class Solution {
-//     public int rob(int[] nums) {
-//         int n = nums.length;
-//         int dp [] = new int[n+1];
-//         Arrays.fill(dp,-1);
-//         return helper(nums,0,n,dp);
-//     }
-//     public int helper(int nums[],int i,int n,int [] dp){
-//         if(i >= n) return 0;
-//         if(dp[i] != -1) return dp[i];
-//         int steal = nums[i]+helper(nums,i+2,n,dp);
-//         int notSteal = helper(nums,i+1,n,dp);
-//         dp[i] = Math.max(steal,notSteal);
-//         return dp[i];
-//     }
-// }
