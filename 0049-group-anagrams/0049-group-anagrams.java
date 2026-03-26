4fb6221ac63757ef42e8cae47d9688f36acc654a
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-03-26 15:03:20 +0530

    Time: 7 ms (75.21%), Space: 49.7 MB (68.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,47 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        String temp[] = new String[n];
-        for(int i = 0;i<n;i++){
-            temp[i] = strs[i];
-        }
-        for(int i = 0;i<n;i++){
-            strs[i] = sortString(strs[i]);
-        }
-        // Arrays.sort(strs);
-        List<List<String>> ans = new ArrayList<>();
-        boolean visited[] = new boolean[n];
-        for(int i = 0;i<n;i++){
-            if(visited[i]) continue;
-            List<String> innerList = new ArrayList<>();
-            innerList.add(temp[i]);
-            for(int j = i+1;j<n;j++){
-                if(strs[i].equals(strs[j])) {
-                    innerList.add(temp[j]);
-                    visited[j]= true;
-                }
+        if(strs.length == 0 || strs == null) return null;
+        Map<String,List<String>> map = new HashMap<>();
+        for(int i = 0;i<strs.length;i++){
+            char innerStr[] = strs[i].toCharArray();
+            Arrays.sort(innerStr);
+            String s = new String(innerStr);
+            if(!map.containsKey(s)){
+                map.put(s,new ArrayList<>());
             }
-            ans.add(innerList);
+            map.get(s).add(strs[i]);
         }
-        return ans;
-    }
-    public String sortString(String strs){
-        char arr[] = strs.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
+        return new ArrayList<>(map.values());
+
+    //     int n = strs.length;
+    //     String temp[] = new String[n];
+    //     for(int i = 0;i<n;i++){
+    //         temp[i] = strs[i];
+    //     }
+    //     for(int i = 0;i<n;i++){
+    //         strs[i] = sortString(strs[i]);
+    //     }
+    //     // Arrays.sort(strs);
+    //     List<List<String>> ans = new ArrayList<>();
+    //     boolean visited[] = new boolean[n];
+    //     for(int i = 0;i<n;i++){
+    //         if(visited[i]) continue;
+    //         List<String> innerList = new ArrayList<>();
+    //         innerList.add(temp[i]);
+    //         for(int j = i+1;j<n;j++){
+    //             if(strs[i].equals(strs[j])) {
+    //                 innerList.add(temp[j]);
+    //                 visited[j]= true;
+    //             }
+    //         }
+    //         ans.add(innerList);
+    //     }
+    //     return ans;
+    // }
+    // public String sortString(String strs){
+    //     char arr[] = strs.toCharArray();
+    //     Arrays.sort(arr);
+    //     return new String(arr);
+    // }
     }
 }
