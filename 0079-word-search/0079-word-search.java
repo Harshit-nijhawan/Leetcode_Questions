018f3d405afbfa8cf5f5d2d694a8859ffe45e187
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-04-03 18:19:50 +0530

    Time: 127 ms (88.93%), Space: 43 MB (67.96%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,40 +1,27 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        boolean isVis[][] = new boolean[n][m];
-        for(int i = 0;i<board.length;i++){
-            for(int j = 0;j<board[0].length;j++){
-                if(dfs(board,word,i,j,n,m,0,isVis)){
-                    return true;
-                }
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(board[i][j] == word.charAt(0))
+                    if(dfs(board,word,i,j,0)) return true;
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word,
-                   int i, int j, int n, int m,
-                   int idx, boolean[][] isVis){
+    public boolean dfs(char[][] board,String word,int i ,int j,int l){
+        if(l == word.length()) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(l)) return false;
+        char c = board[i][j];
+        board[i][j] = '#';
+        boolean found = dfs(board,word,i+1,j,l+1) ||
+                        dfs(board,word,i,j+1,l+1) ||
+                        dfs(board,word,i-1,j,l+1) ||
+                        dfs(board,word,i,j-1,l+1);
 
-    // FIRST check complete match
-    if(idx == word.length()) return true;
+        board[i][j]= c;
+        return found;
 
-    // THEN check boundaries + visited + mismatch
-    if(i < 0 || i >= n || j < 0 || j >= m ||
-       isVis[i][j] || board[i][j] != word.charAt(idx)){
-        return false;
     }
-
-    isVis[i][j] = true;
-
-    boolean found =
-        dfs(board,word,i+1,j,n,m,idx+1,isVis) ||
-        dfs(board,word,i-1,j,n,m,idx+1,isVis) ||
-        dfs(board,word,i,j+1,n,m,idx+1,isVis) ||
-        dfs(board,word,i,j-1,n,m,idx+1,isVis);
-
-    isVis[i][j] = false;   // BACKTRACK
-
-    return found;
-}
 }
