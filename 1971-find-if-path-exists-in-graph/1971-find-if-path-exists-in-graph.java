8692767f27f762d03b8c3a1795c6ddd4b32b8eef
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-05-30 11:26:12 +0530

    Time: 159 ms (27.17%), Space: 279.6 MB (63.97%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,24 +1,27 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> graph = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
         for(int i = 0;i<n;i++){
-            graph.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
-        for(int edge[] : edges){
-            int u = edge[0];
-            int v = edge[1];
+        for(int[] e : edges){
+            int u = e[0];
+            int v = e[1];
 
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
         boolean vis[] = new boolean[n];
-        return helper(graph,source,destination,vis);
+        return helper(adj,source,destination,vis);
+        
     }
-    public boolean helper(List<List<Integer>> edges, int src, int disc,boolean vis[]){
+    public boolean helper(List<List<Integer>> edges,int src,int disc, boolean vis[]){
         if(src == disc) return true;
         vis[src] = true;
         for(int neb : edges.get(src)){
-            if(!vis[neb] && helper(edges,neb,disc,vis)) return true;
+            if(!vis[neb] && helper(edges,neb,disc,vis)){
+                return true;
+            }
         }
         return false;
     }
