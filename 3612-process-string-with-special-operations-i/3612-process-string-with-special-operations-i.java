139a51a60c1f32a9a4bf34ed29592edf89c8cfa1
Harshit Nijhawan <123978424+Harshit-nijhawan@users.noreply.github.com>
2026-06-17 08:16:50 +0530

    Time: 3 ms (100%), Space: 55.5 MB (37.04%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '*')  {
+                if(res.length() > 0) res.deleteCharAt(res.length()-1);
+            }
+            else if(s.charAt(i) == '#') res.append(res.toString());
+            else if(s.charAt(i) == '%') res.reverse();
+            else res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+}
